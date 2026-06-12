d15220ef36c6d156fc132c3bc259f4201a68fd3d
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-13 00:11:42 +0800

    Update project

--- clay_monitor/workers/respiration_eval/BreathabilityWorker.cs
+++ clay_monitor/workers/respiration_eval/BreathabilityWorker.cs
@@ -7,15 +7,6 @@ using Microsoft.Extensions.Options;
 
 namespace respiration_eval;
 
-public record SensorAccuracyConfig
-{
-    public double TemperatureAccuracyC { get; init; } = 0.3;
-    public double HumidityAccuracyPercent { get; init; } = 2.0;
-    public double SensorResponseTimeSeconds { get; init; } = 15.0;
-    public double HysteresisErrorPercent { get; init; } = 1.5;
-    public bool ApplyCorrection { get; init; } = true;
-}
-
 public record BreathabilityInput
 {
     public int SculptureId { get; init; }
