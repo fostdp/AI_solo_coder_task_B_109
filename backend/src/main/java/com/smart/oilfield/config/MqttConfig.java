6e519990f4db6043d0170ae708657d526c687149
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 21:09:58 +0800

    Update project

---
+++ backend/src/main/java/com/smart/oilfield/config/MqttConfig.java
@@ -0,0 +1,36 @@
+package com.smart.oilfield.config;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+
+@Configuration
+public class MqttConfig {
+
+    @Value("${mqtt.broker}")
+    private String broker;
+
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{broker});
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        options.setKeepAliveInterval(30);
+        options.setAutomaticReconnect(true);
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+}
