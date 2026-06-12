6ab73b1579f542fb6e15f55fc97b03eb46b902bc
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 23:34:53 +0800

    Update project

---
+++ clay_monitor/workers/ClayMonitor.Core/Models/MaterialProperties.cs
@@ -0,0 +1,10 @@
+namespace ClayMonitor.Core.Models;
+
+public class MaterialProperties
+{
+    public double ViscosityPaS { get; set; }
+    public double SurfaceTensionNm { get; set; }
+    public double MolarMass { get; set; }
+    public double Density { get; set; }
+    public double TypicalContactAngle { get; set; }
+}
